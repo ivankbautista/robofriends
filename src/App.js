fa98924196c4366c18e685e952e3f58eb2c7bc8d
Ivan <ivankevinbautista@gmail.com>
2019-06-28 11:54:50 +0800

    created a class App that extends Component with a constructor and state, added an onSearchChange, and filteredRobots

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBoxx from './SearchBoxx';
 import { robots } from './robots';
 
-const App = () => {
-    return (
-        <div className='tc'>
-        <h1>RoboFriends</h1>
-        <SearchBoxx />
-        <CardList robots={robots}/>  
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
+            <div className='tc'>
+            <h1>RoboFriends</h1>
+            <SearchBoxx searchChange={this.onSearchChange}/>
+            <CardList robots={filteredRobots}/>  
+            </div>
+        );
+    }
 }
 
 export default App;
